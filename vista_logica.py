65216c6c3671a21b6f7afe6c83999dcc1cfe1ab6
Joaquin H <hernandez.joaquin346@gmail.com>
2025-10-12 03:20:44 -0300

    update

--- vista_logica.py
+++ vista_logica.py
@@ -1,18 +1,26 @@
 from diagrams import Diagram, Cluster, Edge
 from diagrams.programming.framework import React, Spring
 from diagrams.aws.compute import EC2Instance
-with Diagram("Vista Logica - Web Presentation", show=False, outformat='png', direction='TB'):
+graph = {
+    "bgcolor": "white",
+    "pad": "0.5",
+    "splines": "ortho",
+}
+with Diagram("Vista Logica", 
+             show=False, 
+             outformat='png', 
+             direction='LR',
+             graph_attr=graph):
     with Cluster("Front"):
         web = EC2Instance("Web Application")
     with Cluster("Back"):
         backend = EC2Instance("Backend API")
 
-with Diagram("Vista Logica - Backend Layers", show=False, outformat='png'):
-    with Cluster(""):
+    with Cluster("", graph_attr={"labelloc":"c"}):
         rest = EC2Instance("REST Controller")
         service = EC2Instance("Service Layer")
         domain = EC2Instance("Domain Model")
         persistance = EC2Instance("Persistence Layer")
 
-        rest >> service >> domain >> persistance
+    rest >> service >> domain >> persistance
 
